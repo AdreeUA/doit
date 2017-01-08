39b0907c65eea89526d47fb4120cdfd9c0dd372e
Zmievskoy Andrey <a.zmievskoy@digitalwand.ru>
2017-01-08 17:06:23 +0300

    change gulp config

--- desktop/gulpfile.js
+++ desktop/gulpfile.js
@@ -14,24 +14,27 @@ var
     runSequence = require('gulp-run-sequence'),
     del = require('del'),
     watch = require('gulp-watch'),
-    source = require('vinyl-source-stream'),
     browserSync = require('browser-sync'),
     concat = require('gulp-concat'),
-    wiredep = require('wiredep').stream,
+    wiredep = require('wiredep').stream,
     gulpif = require('gulp-if'),
     useref = require('gulp-useref'),
     jade = require('gulp-jade'),
     pipe = require('multipipe'),
-    htmlReplace = require('gulp-html-replace'),
+    cache = require('gulp-cached'),
     plumber = require('gulp-plumber'),
+    sourcemaps = require('gulp-sourcemaps'),
+    lazypipe = require('lazypipe'),
+    spritesmith = require('gulp.spritesmith'),
+    merge = require('merge-stream'),
+    inheritance = require('gulp-jade-inheritance'),
+    filter = require('gulp-filter'),
+    browserify = require('gulp-browserify'),
+    modernizr = require('gulp-modernizr'),
     svgstore = require('gulp-svgstore'),
     svgmin = require('gulp-svgmin'),
     path = require('path'),
-    fs = require('fs'),
-    sourcemaps = require('gulp-sourcemaps'),
-    cache = require('gulp-cached'),
-    inheritance = require('gulp-jade-inheritance'),
-    filter = require('gulp-filter');
+    fs = require('fs');
 
 //===============================================
 // ПУТИ
@@ -41,12 +44,12 @@ var paths = {
 
     scriptsDev: 'app/js/',
     scriptsDist: 'dist/js/',
+    spriteDev: 'app/resources/i/sprite/',
+    imgDev: 'app/resources/i/',
     scss: 'app/scss/',
-    stylesDev: 'app/css/',
     stylesDist: 'dist/css/',
     jade: 'app/jade/',
-    svgIcons: 'app/resources/i/icons',
-    svgImages: 'app/resources/i/svg-images'
+    svgIcons: 'app/resources/i/icons'
 
 };
 
@@ -54,116 +57,22 @@ var paths = {
 // ЗАДАЧИ
 //===============================================
 
-// Компилируем scss и кладем его в dist/css
-gulp.task('sass', function() {
+// генерирует спрайт
+gulp.task('sprite', function () {
 
-    return gulp.src(paths.scss + 'main.scss')
-        .pipe(sassGlobbing())
-        .pipe(sourcemaps.init())
-            .pipe(sass()
-                .on('error', sass.logError))
-            .pipe(prefix("last 2 version", "> 1%", "ie 9", "ie 8"))
-        .pipe(sourcemaps.write())
-        .pipe(rename('styles.css'))
-        .pipe(gulp.dest(paths.stylesDist));
+    var spriteData = gulp.src(paths.spriteDev + '*.png').pipe(spritesmith({
+        imgName: 'sprite.png',
+        imgPath: '../i/sprite.png',
+        cssName: '_sprite.scss',
+        cssFormat: 'scss',
+        padding: 10,
+        cssTemplate: 'handlebarsInheritance.scss.handlebars'
+    }));
 
-});
+    var imgStream = spriteData.img.pipe(gulp.dest(paths.imgDev));
+    var scssStream = spriteData.css.pipe(gulp.dest(paths.scss + 'helpers'));
 
-// Компилирует jade
-gulp.task('jade', function() {
-
-    var pagesList = './pages.json';
-
-    return gulp.src(paths.jade + 'pages/index.jade')
-        .pipe(plumber())
-        .pipe(cache('jade'))
-        .pipe(gulpif(global.watch, inheritance({basedir: paths.jade})))
-        .pipe(filter(file => /app[\\\/]jade[\\\/]pages/.test(file.path)))
-        .pipe(jade({
-            pretty: true,
-            locals: JSON.parse(fs.readFileSync(pagesList, 'utf8'))
-        }))
-        .pipe(rename({dirname: '.'}))
-        .pipe(gulp.dest('dist'));
-
-});
-
-// Компилирует ВЕСЬ jade
-gulp.task('jade-all', function() {
-
-    var pagesList = './pages.json';
-
-    return gulp.src(paths.jade + 'pages/*.jade')
-        .pipe(plumber())
-        .pipe(cache('jade'))
-        .pipe(gulpif(global.watch, inheritance({basedir: paths.jade})))
-        .pipe(filter(file => /app[\\\/]jade[\\\/]pages/.test(file.path)))
-        .pipe(jade({
-            pretty: true,
-            locals: JSON.parse(fs.readFileSync(pagesList, 'utf8'))
-        }))
-        .pipe(rename({dirname: '.'}))
-        .pipe(gulp.dest('dist'));
-
-});
-
-// Склеивает и минифицирует все bower скрипты и стили, а также прописывает путь к новым файлам в html страницах
-gulp.task('useref-all', function () {
-
-    return gulp.src('dist/index.html')
-        .pipe(useref())
-        .pipe(cache('useref'))
-        .pipe(gulpif('*.js', pipe(
-            // убирает суффикс .min и сохраняет не минифицированную версию
-            rename(function (path) {
-                path.basename = path.basename.slice(0, path.basename.length - 4);
-            }),
-            gulp.dest('dist'),
-            uglify(),
-            rename({
-                suffix: '.min'
-            }) // снова добавляет суффикс .min
-        )))
-        .pipe(gulpif('*.css', pipe(
-            minifyCss()
-        )))
-        .pipe(gulp.dest('dist'));
-
-});
-
-// Склеивает и минифицирует все bower скрипты и стили, а также прописывает путь КО ВСЕМ файлам в html страницах
-gulp.task('useref', function () {
-
-    return gulp.src('dist/*.html')
-        .pipe(useref())
-        .pipe(cache('useref'))
-        .pipe(gulpif('*.js', pipe(
-            // убирает суффикс .min и сохраняет не минифицированную версию
-            rename(function (path) {
-                path.basename = path.basename.slice(0, path.basename.length - 4);
-            }),
-            gulp.dest('dist'),
-            uglify(),
-            rename({
-                suffix: '.min'
-            }) // снова добавляет суффикс .min
-        )))
-        .pipe(gulpif('*.css', pipe(
-            minifyCss()
-        )))
-        .pipe(gulp.dest('dist'));
-
-});
-
-// Подключает bower файлы к jade шаблонам
-gulp.task('wiredep', function() {
-
-    return gulp.src(paths.jade + 'partials/scripts.jade')
-        .pipe(wiredep({
-            ignorePath: '../../',
-            exclude: ['bower_components/picturefill']
-        }))
-        .pipe(gulp.dest(paths.jade + 'partials/'));
+    return merge(imgStream, scssStream);
 
 });
 
@@ -190,23 +99,90 @@ gulp.task('svg:icons', function () {
         .pipe(gulp.dest('app/resources/i'));
 });
 
-// Генерирует svg спрайт из иконок, которым задан цвет
-gulp.task('svg:images', function () {
-    return gulp.src(paths.svgImages + '/*.svg')
-        .pipe(svgmin(function (file) {
-            var prefix = path.basename(file.relative, path.extname(file.relative));
-            return {
-                plugins: [{
-                    cleanupIDs: {
-                        prefix: prefix + '-',
-                        minify: true
-                    }
-                }]
-            }
+// Компилируем scss и кладем его в dist/css
+gulp.task('sass', function() {
+
+    return gulp.src(paths.scss + 'main.scss')
+        .pipe(sassGlobbing())
+        .pipe(sourcemaps.init())
+            .pipe(sass()
+                .on('error', sass.logError))
+            .pipe(prefix("last 2 version", "> 1%", "ie 9"))
+        .pipe(sourcemaps.write())
+        .pipe(rename('styles.css'))
+        .pipe(gulp.dest(paths.stylesDist));
+
+});
+
+// Компилирует jade
+gulp.task('jade', function() {
+
+    var pagesList = './pages.json';
+
+    return gulp.src(paths.jade + 'pages/*.jade')
+        .pipe(plumber())
+        .pipe(cache('jade'))
+        .pipe(gulpif(global.watch, inheritance({basedir: paths.jade})))
+        .pipe(filter(file => /app[\\\/]jade[\\\/]pages/.test(file.path)))
+        .pipe(jade({
+            pretty: true,
+            locals: JSON.parse(fs.readFileSync(pagesList, 'utf8'))
         }))
-        .pipe(rename({prefix: 'img-'}))
-        .pipe(svgstore())
-        .pipe(gulp.dest('app/resources/i'));
+        .pipe(rename({dirname: '.'}))
+        .pipe(gulp.dest('dist'));
+
+});
+
+ gulp.task('scripts', function() {
+    return gulp.src(paths.scriptsDev + 'entry.js')
+        .pipe(plumber())
+        .pipe(browserify({
+            debug : true
+        }))
+        .pipe(rename('scripts.js'))
+        .pipe(gulp.dest(paths.scriptsDist));
+});
+
+// Подключает bower файлы к html файлам
+gulp.task('wiredep', function() {
+
+    return gulp.src(paths.jade + '**/*.jade')
+        .pipe(plumber())
+        .pipe(wiredep({
+            ignorePath: '../../'
+        }))
+        .pipe(gulp.dest(paths.jade));
+
+});
+
+// Склеивает и минифицирует все bower скрипты и стили, а также прописывает путь к новым файлам в html страницах
+gulp.task('useref', function () {
+
+    return gulp.src('dist/*.html')
+        .pipe(useref({}))
+        .pipe(cache('useref'))
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest('dist'));
+
+});
+
+// Сборка файла modernizr
+gulp.task('modernizr', function() {
+    return gulp.src(paths.scriptsDev + '*.js')
+        .pipe(modernizr('modernizr.min.js', {
+            'options': [
+                'setClasses'
+            ],
+            'tests': [
+                'flexbox',
+                'csscalc',
+                'objectfit'
+            ],
+            'uglify': true
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.scriptsDist));
 });
 
 // Копируем всё из папки resources в dist
@@ -252,18 +228,28 @@ gulp.task('watch', function() {
         runSequence('jade', browserSync.reload);
     });
 
-    watch(paths.scriptsDev + '**/*.js', browserSync.reload);
+    watch(paths.scriptsDev + '**/*.js', function() {
+        runSequence('scripts', browserSync.reload);
+    });
+
+    watch('app/resources/**/*', function(event) {
+
+        if (event) {
+            runSequence('copy', browserSync.reload);
+        }
+
+    });
+
+    watch(paths.spriteDev + '/**/*.png', function(event) {
+
+        if (event) {
+                runSequence('sprite', 'copy');
+        }
+
+    });
 
     watch(paths.svgIcons + '**/*.svg', function() {
         runSequence('svg:icons', browserSync.reload);
-    });
-
-    watch(paths.svgImages + '**/*.svg', function() {
-        runSequence('svg:images', browserSync.reload);
-    });
-
-    watch('app/resources/**/*', function() {
-        runSequence('copy', browserSync.reload);
     });
 
     watch('bower.json', function() {
@@ -275,38 +261,29 @@ gulp.task('watch', function() {
 // Сборка на продакшн
 gulp.task('build', ['clean'], function() {
     runSequence(
+        'modernizr',
         'svg:icons',
-        'svg:images',
         'wiredep',
-        'copy',
         'jade',
-        'sass',
-        'useref'
-    )
-});
-
-// Полная сборка на продакшн
-gulp.task('superbuild', ['clean'], function() {
-    runSequence(
-        'svg:icons',
-        'svg:images',
-        'wiredep',
+        'sprite',
         'copy',
-        'jade-all',
         'sass',
-        'useref-all'
-    )
+        'scripts',
+        'useref'
+    );
 });
 
 // Задача по умолчанию
 gulp.task('default', ['clean'], function() {
     runSequence(
+        'modernizr',
         'svg:icons',
-        'svg:images',
         'wiredep',
+        'jade',
+        'sprite',
         'copy',
-        'jade-all',
         'sass',
+        'scripts',
         'server',
         'watch'
     )
